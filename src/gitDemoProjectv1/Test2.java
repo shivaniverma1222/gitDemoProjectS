a3ae294a2b371bb87a9dd447bf6dd7ca66f7f27f
sachin <sachin@LAPTOP-1M809VKI>
2020-12-13 02:07:42 +0530

    Commiting test2.java to my branch dev_shivani

---
+++ src/gitDemoProjectv1/Test2.java
@@ -0,0 +1,19 @@
+package gitDemoProjectv1;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("eating something.....");
+		}
+		else
+		{
+			System.out.println("Doing Work ......");
+		}
+	}
+
+}
